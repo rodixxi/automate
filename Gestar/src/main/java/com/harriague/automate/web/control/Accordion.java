1300c8f26443e4d4489d555699ad1fabe8d99595
Rodrigo Crespillo <rcrespillo@harriague.com.ar>
2017-07-12 12:49:22 -0300

    Accordion path

--- Gestar/src/main/java/com/harriague/automate/web/control/Accordion.java
+++ Gestar/src/main/java/com/harriague/automate/web/control/Accordion.java
@@ -1,4 +1,15 @@
 package com.harriague.automate.web.control;
 
-public class Accordion {
+import org.openqa.selenium.By;
+
+public class Accordion extends Control{
+    public Accordion(String nombre, String etiqueta) {
+        super(nombre, etiqueta);
+    }
+
+    public Accordion(String etiqueta) {
+        super(etiqueta);
+        String path = "//p['" + etiqueta + "']/ancestor::h3[1]";
+        setXpathSelectorByName(By.xpath(path));
+    }
 }
