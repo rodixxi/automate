874592eb992b581db0b832e15d9adbe9495fb550
Rodrigo Crespillo <rcrespillo@harriague.com.ar>
2017-07-12 12:41:40 -0300

    Checkbox xpath

--- Gestar/src/main/java/com/harriague/automate/web/control/Checkbox.java
+++ Gestar/src/main/java/com/harriague/automate/web/control/Checkbox.java
@@ -1,4 +1,18 @@
 package com.harriague.automate.web.control;
 
-public class Checkbox {
+import org.openqa.selenium.By;
+
+public class Checkbox extends Control{
+
+
+    public Checkbox(String nombre, String etiqueta) {
+        super(nombre, etiqueta);
+    }
+
+    public Checkbox(String etiqueta) {
+        super(etiqueta);
+        String xpath = "//span[text()='" + etiqueta + "']/ancestor::td[1]/following::td[1]/span/input[@type='checkbox']";
+        setXpathSelectorByName(By.xpath(xpath));
+    }
+
 }
