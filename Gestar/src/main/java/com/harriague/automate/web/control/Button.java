e55eaa0a1ee7cfcb093c6af0d34e9d53b66abf26
Rodrigo Crespillo <rcrespillo@harriague.com.ar>
2017-07-12 12:12:54 -0300

    Button xpath added

--- Gestar/src/main/java/com/harriague/automate/web/control/Button.java
+++ Gestar/src/main/java/com/harriague/automate/web/control/Button.java
@@ -1,4 +1,26 @@
 package com.harriague.automate.web.control;
 
-public class Button {
+import org.openqa.selenium.By;
+
+public class Button extends Control {
+
+    private String text = "";
+    private String buttonXpath= "//button[text()='"+ text +"']";
+
+
+    public Button(String nombre, String etiqueta) {
+        super(nombre, etiqueta);
+    }
+
+    public Button(String text) {
+        super("");
+        this.text = text;
+        getButtonXpath();
+    }
+
+
+    public void getButtonXpath(){
+        buttonXpath = "//button[text()='"+ text +"']";
+        setXpathSelectorByName(By.xpath(buttonXpath));
+    }
 }
